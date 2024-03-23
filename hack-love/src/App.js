a0f742750aef95c88bef1a0b0742cdb51ba3958c
WonjungWoo <112631065+WonjungWoo@users.noreply.github.com>
2024-03-23 17:26:12 +0900

    minor changes

--- hack-love/src/App.js
+++ hack-love/src/App.js
@@ -4,12 +4,14 @@ import { Route, Routes } from "react-router-dom";
 
 import Home from "./routes/Home";
 import Creation from "./routes/Creation";
+import Result from "./routes/Result";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/create_room" element={<Creation />} />
+      <Route path="/result" element={<Result />} />
     </Routes>
   );
 }
---
+++ hack-love/src/routes/Result.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Circle.css';
+import photo from '../img/photo_1.PNG';
+
+const Result = () => {
+  return (
+    <div style={{ position: 'absolute', top: '-40%', width: '60vw' }}>
+      <div className="circle" style={{width: '100%'}}>
+      </div>
+    </div>
+  );
+};
+
+export default Result;
