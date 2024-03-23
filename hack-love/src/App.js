34def269beec2cd7cd87c41775ea3714753bfe71
WonjungWoo <112631065+WonjungWoo@users.noreply.github.com>
2024-03-23 16:01:42 +0900

    folders added

--- hack-love/src/App.js
+++ hack-love/src/App.js
@@ -3,11 +3,13 @@ import { Route, Routes } from "react-router-dom";
 
 
 import Home from "./routes/Home";
+import Creation from "./routes/Creation";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/create_room" element={<Creation />} />
     </Routes>
   );
 }
